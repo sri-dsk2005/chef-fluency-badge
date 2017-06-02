5298fbaf5ebe06b2180a63b41fa012b8e05121d0
srikanth <sriredspot@gmail.com>
2017-06-02 16:05:51 +0000

    new cookbook apache is added

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,7 @@
+name 'apache'
+maintainer 'srikanth'
+maintainer_email 'sriredspot@gmail.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
